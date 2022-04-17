e554e4070c3ba3abfb38ee9f79dc803585547705
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-17 15:29:56 +0200

    One Quick Change

--- Sekcja 1 Getting Started with Typescript/index.ts
+++ Sekcja 1 Getting Started with Typescript/index.ts
@@ -3,5 +3,15 @@ import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const data = response.data;
+
+  const id = data.ID;
+  const title = data.Title;
+  const finished = data.finished;
+
+  console.log(
+    `Todo with id: ${id}
+  title: ${title}
+  finished: ${finished}`
+  );
 });
