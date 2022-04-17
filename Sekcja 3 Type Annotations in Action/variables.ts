a99d4f2a559a7b1787b5f87a58e4d00265500b3d
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-17 18:14:13 +0200

    Object Literal Annotations

--- Sekcja 3 Type Annotations in Action/variables.ts
+++ Sekcja 3 Type Annotations in Action/variables.ts
@@ -6,3 +6,14 @@ let notMouch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+let colors: string[] = ["red", "blue", "green"];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
