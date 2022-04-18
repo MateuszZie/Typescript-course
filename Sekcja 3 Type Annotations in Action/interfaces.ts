ccc5d2b8f1f47963cbb74ce6a703cf56027cb107
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-18 16:56:52 +0200

    Syntax Around Interfaces

--- Sekcja 3 Type Annotations in Action/interfaces.ts
+++ Sekcja 3 Type Annotations in Action/interfaces.ts
@@ -2,16 +2,20 @@ interface Vehicle {
   name: string;
   year: number;
   brooken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: "civic",
   year: 2000,
   brooken: true,
+  summary(): string {
+    return `${this.name + " " + this.year + " " + this.brooken}`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.name, vehicle.year, vehicle.brooken);
+  console.log(vehicle.summary);
 };
 
 printVehicle(oldCivic);
