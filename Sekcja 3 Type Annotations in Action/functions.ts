ab74cafe8ca325e782922024fc44d8a316ee9630
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-18 11:57:17 +0200

    Void and Never

--- Sekcja 3 Type Annotations in Action/functions.ts
+++ Sekcja 3 Type Annotations in Action/functions.ts
@@ -7,3 +7,17 @@ function substract(a: number, b: number): number {
 const divide = function (a: number, b: number): number {
   return a / b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwErrorIf = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
