d10c8e0b217cb816f90bb133cef46e687b38a0c4
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-18 11:49:39 +0200

    Annotations for Anonymous Functions

--- Sekcja 3 Type Annotations in Action/functions.ts
+++ Sekcja 3 Type Annotations in Action/functions.ts
@@ -1,3 +1,9 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+function substract(a: number, b: number): number {
+  return a - b;
+}
+const divide = function (a: number, b: number): number {
+  return a / b;
+};
