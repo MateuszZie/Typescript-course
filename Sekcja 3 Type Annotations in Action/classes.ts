72a7d13691c20cb6015dc9ea604cafd94a442973
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-19 20:50:37 +0200

    Instance Method Modifiers

--- Sekcja 3 Type Annotations in Action/classes.ts
+++ Sekcja 3 Type Annotations in Action/classes.ts
@@ -1,20 +1,20 @@
 class Vehicle {
-  drive(): void {
-    console.log("chogga chugga");
-  }
-
-  hunk(): void {
+  protected hunk(): void {
     console.log("beep");
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+
+  startDriving(): void {
+    this.drive();
+    this.hunk();
   }
 }
 
 const car = new Car();
 
-car.drive();
-car.hunk();
+car.startDriving();
