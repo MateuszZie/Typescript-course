b0734ed4be365457309b9f2d043fcdc84cac4d63
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-22 18:24:04 +0200

    Why Use Private Modifiers? Here's Why

--- Section 9 Design Patterns with Typescript/src/index.ts
+++ Section 9 Design Patterns with Typescript/src/index.ts
@@ -1,14 +1,8 @@
-/// <reference types="@types/google.maps" />
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const map = new CustomMap("map");
