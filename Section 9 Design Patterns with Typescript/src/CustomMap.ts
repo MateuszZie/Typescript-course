04af7f4cf704e3253d63f68e579e868a6d6f7f54
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-23 11:37:22 +0200

    One Possible Solution

--- Section 9 Design Patterns with Typescript/src/CustomMap.ts
+++ Section 9 Design Patterns with Typescript/src/CustomMap.ts
@@ -14,22 +14,12 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(user: User | Company): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: user.location.lat,
         lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
       },
     });
   }
