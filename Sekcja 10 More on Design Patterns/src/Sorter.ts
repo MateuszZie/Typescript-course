c4950cc3b273b39afd0c7548c5254691d77bbc52
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-24 18:43:36 +0200

    Extracting Key Logic

---
+++ Sekcja 10 More on Design Patterns/src/Sorter.ts
@@ -0,0 +1,21 @@
+export class Sorter {
+  constructor(public collection: number[] | string) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection instanceof Array) {
+          const temp = this.collection[j];
+          if (this.collection[j] > this.collection[j + 1]) {
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+        if (typeof this.collection === "string") {
+        }
+      }
+    }
+  }
+}
