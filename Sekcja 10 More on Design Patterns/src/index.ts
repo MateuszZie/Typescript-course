a492f629117de461396a48000d51e3c9a68b4841
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-24 16:25:06 +0200

    Sorter Scaffolding

--- Sekcja 10 More on Design Patterns/src/index.ts
+++ Sekcja 10 More on Design Patterns/src/index.ts
@@ -1,2 +1,9 @@
-console.log("hello there");
-console.log("new line");
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([3, -2, 5, 8, 4]);
+sorter.sort();
+console.log(sorter.collection);
