d12eea3de5b5dbd2c3715db99cd6594cc2d5e577
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-24 19:38:00 +0200

    Interface Definition

--- Sekcja 10 More on Design Patterns/src/Sorter.ts
+++ Sekcja 10 More on Design Patterns/src/Sorter.ts
@@ -1,7 +1,11 @@
-import { NumbersCollection } from "./NumbersCollection";
+export interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightInde: number): boolean;
+  swap(leftIndex: number, rightInde: number): void;
+}
 
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
