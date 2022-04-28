8942c8f62529d0e5f12af7096abde96bae7ee55e
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-28 20:50:36 +0200

    Extracting CSV Reading

--- Sekcja 11 Reusable Code/src/index.ts
+++ Sekcja 11 Reusable Code/src/index.ts
@@ -1,11 +1,8 @@
 import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => row.split(","));
+const matches = new CsvFileReader("football.csv");
+matches.read();
 
 enum MatchResault {
   HomeWin = "H",
@@ -15,7 +12,7 @@ enum MatchResault {
 
 let manUnitedWin = 0;
 
-for (let match of matches) {
+for (let match of matches.data) {
   if (match[1] === "Man United" && match[5] === MatchResault.HomeWin) {
     manUnitedWin++;
   } else if (match[2] === "Man United" && match[5] === MatchResault.AwayWin) {
