2bd25694925f4e183c3e6813e2bd67be571b21f0
Mateusz Ziebura <mateusz_860@op.pl>
2022-05-04 18:44:56 +0200

    Retrieving User Properties

--- Sekcja 13 Let's Build a Web Framework/src/index.ts
+++ Sekcja 13 Let's Build a Web Framework/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hi there");
+import { User } from "./models/User";
+
+const user = new User({ age: 36, name: "Mateusz" });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
