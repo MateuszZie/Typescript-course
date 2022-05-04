0d32030d39b4fbf72f3298d6302ec0b262766c89
Mateusz Ziebura <mateusz_860@op.pl>
2022-05-04 18:53:17 +0200

    Optional Interface Properties

--- Sekcja 13 Let's Build a Web Framework/src/index.ts
+++ Sekcja 13 Let's Build a Web Framework/src/index.ts
@@ -1,6 +1,8 @@
 import { User } from "./models/User";
 
-const user = new User({ age: 36, name: "Mateusz" });
+const user = new User({});
+
+user.set({ name: "Mateusz" });
 
 console.log(user.get("name"));
 console.log(user.get("age"));
