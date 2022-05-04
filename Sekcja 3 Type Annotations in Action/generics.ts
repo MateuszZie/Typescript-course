90010cb0cde1b0314c91e107a22e22c979148399
Mateusz Ziebura <mateusz_860@op.pl>
2022-05-04 17:54:44 +0200

    Function Generics

--- Sekcja 3 Type Annotations in Action/generics.ts
+++ Sekcja 3 Type Annotations in Action/generics.ts
@@ -9,3 +9,11 @@ class ArrayofAnything<T> {
 const stringArr = new ArrayofAnything<string>(["a", "v", "c"]);
 
 const stringArr2 = new ArrayofAnything(["a", "v", "c"]);
+
+function printAything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAything([1, 2, 3]);
