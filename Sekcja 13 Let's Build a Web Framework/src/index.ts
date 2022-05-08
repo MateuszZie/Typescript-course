f3d3d97c46f2ae295d8306a546f1039f776b257e
Mateusz Ziebura <mateusz_860@op.pl>
2022-05-08 09:22:17 +0200

    Saving Data

--- Sekcja 13 Let's Build a Web Framework/src/index.ts
+++ Sekcja 13 Let's Build a Web Framework/src/index.ts
@@ -2,10 +2,22 @@ import { User, UserProps } from "./models/User";
 
 const user = new User<UserProps>({ id: 1, name: "mat", age: 22 });
 
-console.log(user.get("name"));
+console.log(user.get("id"));
 
 user.on("click", () => {
   console.log("Clicked");
 });
 
+user.on("change", () => {
+  console.log("User was changed");
+});
+
 user.trigger("click");
+
+user.set({ name: "mateusz", age: 36 });
+
+const user2 = new User<UserProps>({ id: 2 });
+
+user2.fetch();
+
+console.log(user2);
--- Sekcja 13 Let's Build a Web Framework/src/models/User.ts
+++ Sekcja 13 Let's Build a Web Framework/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
+import { AxiosResponse } from "axios";
 
 export interface UserProps {
   id?: number;
@@ -29,5 +30,33 @@ export class User<T> {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (!id) {
+      throw new Error("Cant fetch without id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse) => {
+      this.attributes.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse) => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
   }
 }
