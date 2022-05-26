dbb304adfffdd8efdb3476520d79df2725d3e9b5
Mateusz Ziebura <mateusz_860@op.pl>
2022-05-26 20:36:53 +0200

    The UserForm's Render Method

--- Sekcja 13 Let's Build a Web Framework/src/views/Userform.ts
+++ Sekcja 13 Let's Build a Web Framework/src/views/Userform.ts
@@ -1,10 +1,16 @@
 export class UserForm {
-  parent: Element;
+  constructor(public parent: Element) {}
 
   template(): string {
     return `<div> <h1>
         User Form<h1/>
         <input />
         </div>`;
+  }
+
+  render(): void {
+    const templateElement = document.createElement("template");
+    templateElement.innerHTML = this.template();
+    this.parent.append(templateElement.content);
   }
 }
