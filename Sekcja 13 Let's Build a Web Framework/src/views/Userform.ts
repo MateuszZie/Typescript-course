e2a2511b154c77d6dbdf49d4ca1dddb265e0ffc8
Mateusz Ziebura <mateusz_860@op.pl>
2022-05-28 09:04:38 +0200

    Binding Event Handlers

--- Sekcja 13 Let's Build a Web Framework/src/views/Userform.ts
+++ Sekcja 13 Let's Build a Web Framework/src/views/Userform.ts
@@ -3,17 +3,34 @@ export class UserForm {
 
   eventMap(): { [key: string]: () => void } {
     return {
-      "button:click": this.onButtonClick,
+      "click:button": this.onButtonClick,
+      "mouseenter:h1": this.mouseHoverH1,
     };
+  }
+
+  eventBanding(fragment: DocumentFragment) {
+    const eventMap = this.eventMap();
+
+    for (let eventKay in eventMap) {
+      const [eventName, selector] = eventKay.split(":");
+
+      fragment.querySelectorAll(selector).forEach((elemnet) => {
+        elemnet.addEventListener(eventName, eventMap[eventKay]);
+      });
+    }
   }
 
   onButtonClick(): void {
     console.log("Hi there!");
   }
 
+  mouseHoverH1(): void {
+    console.log("H1 howered");
+  }
+
   template(): string {
     return `<div> <h1>
-        User Form<h1/>
+        User Form</h1>
         <input /> <button>Click Me</button>
         </div>`;
   }
@@ -21,6 +38,7 @@ export class UserForm {
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+    this.eventBanding(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
