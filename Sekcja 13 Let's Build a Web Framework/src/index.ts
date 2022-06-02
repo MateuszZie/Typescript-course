c759cd7d207614eddcfee049752773aa48373596
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-02 18:53:54 +0200

    Testing Region Mapping

--- Sekcja 13 Let's Build a Web Framework/src/index.ts
+++ Sekcja 13 Let's Build a Web Framework/src/index.ts
@@ -1,13 +1,14 @@
 import { User } from "./models/User";
-import { UserForm } from "./views/Userform";
+import { UserEdit } from "./views/UserEdit";
 
 const user = User.buildUser({ name: "Name", age: 20 });
 
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error("Root not found");
 }
--- Sekcja 13 Let's Build a Web Framework/src/views/UserEdit.ts
+++ Sekcja 13 Let's Build a Web Framework/src/views/UserEdit.ts
@@ -2,6 +2,9 @@ import { User, UserProps } from "../models/User";
 import { View } from "./View";
 
 export class UserEdit extends View<User, UserProps> {
+  regionMap(): { [key: string]: string } {
+    return { UserShow: ".user-show", UserForm: ".user-form" };
+  }
   template(): string {
     return `
         <div>
