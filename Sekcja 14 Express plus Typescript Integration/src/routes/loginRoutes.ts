665f0ae061a5cc084633308169d4ef4cef6019c9
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-04 17:19:57 +0200

    Wiring Up Sessions

--- Sekcja 14 Express plus Typescript Integration/src/routes/loginRoutes.ts
+++ Sekcja 14 Express plus Typescript Integration/src/routes/loginRoutes.ts
@@ -23,10 +23,11 @@ router.get("/login", (req: Request, res: Response) => {
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email.toUpperCase());
+  if (email && password && email === "hi@hi.com" && password === "password") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
   } else {
-    res.send("Email must be provided");
+    res.send("Inwalid email or password");
   }
 });
 
