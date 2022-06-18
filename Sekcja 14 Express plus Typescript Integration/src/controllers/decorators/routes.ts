1006eb6117d4967099c6009a8a7c2fb8a35ff26d
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-18 10:20:18 +0200

    Defining a RouteBinder

--- Sekcja 14 Express plus Typescript Integration/src/controllers/decorators/routes.ts
+++ Sekcja 14 Express plus Typescript Integration/src/controllers/decorators/routes.ts
@@ -1,7 +1,16 @@
 import "reflect-metadata";
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
+function routerBinding(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
   };
 }
+
+export const get = routerBinding("get");
+export const post = routerBinding("post");
+export const put = routerBinding("put");
+export const del = routerBinding("delete");
+export const path = routerBinding("path");
