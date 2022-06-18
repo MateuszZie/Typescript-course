8274197917217383f246a221dd2c8bbd0ac1375a
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-18 17:10:34 +0200

    Handling Component State

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -6,8 +6,22 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  increment = () => {
+    this.setState({ counetr: this.state.counter++ });
+  };
+  decrement = () => {
+    this.setState({ counetr: this.state.counter-- });
+  };
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
