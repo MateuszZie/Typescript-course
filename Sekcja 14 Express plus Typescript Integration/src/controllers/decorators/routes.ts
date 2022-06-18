5161e466ea9d57cca00d93403e38e1291dcfb691
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-18 16:12:03 +0200

    Using Property Descriptors for Type Checking

--- Sekcja 14 Express plus Typescript Integration/src/controllers/decorators/routes.ts
+++ Sekcja 14 Express plus Typescript Integration/src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
+import { RequestHandler } from "express";
 import "reflect-metadata";
 import { MetaDataKeys } from "./MetaDataKeys";
 import { Methods } from "./Methods";
 
+interface RouterHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
+
 function routerBinding(method: string) {
   return function (path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: RouterHandlerDescriptor) {
       Reflect.defineMetadata(MetaDataKeys.path, path, target, key);
       Reflect.defineMetadata(MetaDataKeys.method, method, target, key);
     };
