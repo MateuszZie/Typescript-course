3dc3b5ee4a282e8baa6dbaf46a8683b5d9eec531
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-21 21:19:51 +0200

    Action Types Enum

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { Types } from "./types";
 
 const url = "https://jsonplaceholder.typicode.com/todos";
 
+interface ToDo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<ToDo>(url);
 
     dispatch({
-      type: "FETCH_TODOS",
+      type: Types.fetchTodos,
       peyload: response.data,
     });
   };
