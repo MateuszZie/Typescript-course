0b7f72fb808e11a4c2a582970dd5ad6cb67c35a4
Mateusz Ziebura <mateusz_860@op.pl>
2022-06-23 21:20:48 +0200

    Type Guards in Reducers

--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -4,6 +4,8 @@ export const todosReducer = (state: ToDo[] = [], action: Action) => {
   switch (action.type) {
     case Types.fetchTodos:
       return action.peyload;
+    case Types.deleteTodo:
+      return state.filter((todo: ToDo) => todo.id !== action.payload);
     default:
       return state;
   }
